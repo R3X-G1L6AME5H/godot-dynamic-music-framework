6574db7e119be60bb262ff9aa732602aa0290fab
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2021-08-10 19:53:12 +0200

    Wired the Blackboard singleton in the plugin.cfg

--- addons/DMF/plugin.gd
+++ addons/DMF/plugin.gd
@@ -1,8 +1,8 @@
 tool
 extends EditorPlugin
 
-#func _enter_tree():
-#	pass
+func _enter_tree():
+	add_autoload_singleton("Blackboard", "res://addons/DMF/singletons/Blackboard.gd")
 
-#func _exit_tree():
-#	pass
+func _exit_tree():
+	remove_autoload_singleton("Blackboard")

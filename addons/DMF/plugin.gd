2d2d9fd27fddab3fa0dc0ef71e403a6d5569036d
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2021-08-12 19:02:09 +0200

    added the two autoloads

--- addons/DMF/plugin.gd
+++ addons/DMF/plugin.gd
@@ -3,6 +3,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_autoload_singleton("Blackboard", "res://addons/DMF/singletons/Blackboard.gd")
+	add_autoload_singleton("MusicController", "res://addons/DMF/singletons/MusicController.tscn")
 
 func _exit_tree():
 	remove_autoload_singleton("Blackboard")
+	remove_autoload_singleton("MusicController")

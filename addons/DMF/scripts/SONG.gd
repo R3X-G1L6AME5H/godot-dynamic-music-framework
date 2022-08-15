da887d9f715fe2dc37a644471d2b8d2f6a5eda7e
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2022-08-15 03:30:12 +0000

    Stamp&Document (pt.5) - DMF Song

--- addons/DMF/scripts/SONG.gd
+++ addons/DMF/scripts/SONG.gd
@@ -2,12 +2,34 @@ tool
 extends DynamicMusicFramework
 class_name DMFSong
 
+"""
+	Dynamic Music Framework Song Node
+		by Nemo Czanderlitch/Nino Čandrlić
+			@R3X-G1L       (godot assets store)
+			R3X-G1L6AME5H  (github)
+
+	A (child) data node used by the DMF Playlist Generator. Holds vital information
+	about the song being played. As all child nodes position themselves on the timeline
+	using bar numbers, it is essencial that bpm and time signature match those of
+	the audio track. In addition, it specifies the first segment that gets played.
+"""
+
+## Self-explainatory variables
 export (float) var bpm = 140
 export (int) var timesig_numerator   = 4
 export (int) var timesig_denominator = 4
+## The first segment that gets played when the song starts
 var default_segment = ""
+
+
+
+## A variable holding the data of all the segments in editor
 var hint = ""
 
+
+
+####  B O I L E R P L A T E  #####
+##################################
 func _get( property : String ):
 	match property:
 		"default_segment" :
@@ -38,6 +60,9 @@ func _ready():
 	property_list_changed_notify()
 	_get_hint()
 
+
+
+## Fetches the available Segments
 func _get_hint():
 	hint = ""
 	for child in get_children():

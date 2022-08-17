02e457e22babff24a9f7a78db7e0fdef3acd88e1
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2022-08-17 11:10:10 +0000

    Stamp&Document (pt.7) - Adaptation of the Blackboard from ThisIsVini

--- addons/DMF/singletons/Blackboard.gd
+++ addons/DMF/singletons/Blackboard.gd
@@ -1,19 +1,32 @@
 extends Node
 
 """
-	Simple Blackboard Implementation by Nino Candrlic @R3X_G1L6AME5H
+	A Simple Blackboard Singleton
+		adapted from "This Is Vini"'s code
+			(https://github.com/viniciusgerevini/godot-behavior-tree-example/blob/master/addons/behavior_tree/blackboard.gd)
+		https://www.youtube.com/c/ThisIsVini  (Youtube)
+		https://github.com/viniciusgerevini   (github)
+
+	A simple blackboard. This singleton is where the MusicController Singleton gets its
+	data from. All the properties listed in the Playlist Generator, also reffer to data
+	within this blackboard.
 """
 
 var blackboard = {}
 
+
+## A signal for any value changes
+## 		(for updating MusicController Singleton's state)
 signal blackboard_value_set( key )
+
+
 
 func set(key, value, blackboard_name = 'default'):
 	if not blackboard.has(blackboard_name):
 		blackboard[blackboard_name] = {}
 
 	blackboard[blackboard_name][key] = value
-	emit_signal("blackboard_value_set", key)
+	emit_signal("blackboard_value_set", key) ## Update MusicController's state
 
 
 func get(key, blackboard_name = 'default'):
